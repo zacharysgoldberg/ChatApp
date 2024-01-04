e447700eb14b18890ed30485b1002d1bdfecc419
Zachary.Goldberg <zachary.goldberg@hii-tsd.com>
2024-01-03 16:21:40 -0800

    Added angular skeleton

--- API/Program.cs
+++ API/Program.cs
@@ -5,12 +5,18 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+string connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<API.Data.DataContext>(opt => 
 {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+    opt.UseSqlite(connectionString);
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+// Confugure the HTTP request pipeline
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
